6597e3dcbca4d15a46a6427f571404066fd08ca5
Nii-101 <noelamptey101@gmail.com>
2024-07-29 07:52:23 +0000

    fixing text responsiveness for Homepage animation

--- calabashe-app/src/pages/Homepage.jsx
+++ calabashe-app/src/pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <main className='h-[100vh] flex flex-col px-2 items-center justify-center'>
 
           {/* <Review/>        */}
-          <h1 className="z-1 text-6xl font-black animate-bounce ">Calabas<span className="text-[#0B7F85]">He</span></h1> 
+          <h1 className="z-1 text-5xl sm:text-6xl font-black animate-bounce ">Calabas<span className="text-[#0B7F85]">He</span></h1> 
 
         </main>
         <Footer/>
