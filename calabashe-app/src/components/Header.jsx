d33e9aa8f56fd097f6477cd120e2ee684dba2c3f
Nii-101 <noelamptey101@gmail.com>
2024-08-13 17:16:23 +0000

    fix: reduced font-size of nav menu links

--- calabashe-app/src/components/Header.jsx
+++ calabashe-app/src/components/Header.jsx
@@ -3,8 +3,8 @@ import Menu from './menuButton';
 import LgScreenMenu from './menu-lg';
 const Header = () => {
   return ( 
-    <header className="z-10 subpixel-antialiased select-none fixed top-0 font-poppins w-full max-h-[50px] text-white/100 bg-black flex items-center justify-between items-center p-2 md:p-4 px-2 lg:px-[4%]">
-      <h1 className="text-xl lg:text-3xl font-bold"><Link to="/home">Calabas<span className="text-[#04DA8D]">He</span></Link></h1>
+    <header className="z-10 subpixel-antialiased select-none fixed top-0 font-poppins w-full max-h-[50px] text-white/100 bg-black flex items-center justify-between items-center p-2 md:p-4 px-2 md:px-12 lg:px-20 xl:px-28">
+      <h1 className="text-xl lg:text-2xl font-bold"><Link to="/home">Calabas<span className="text-[#04DA8D]">He</span></Link></h1>
       <nav >
         <Menu/>
         <LgScreenMenu/>
--- calabashe-app/src/components/menu-lg.jsx
+++ calabashe-app/src/components/menu-lg.jsx
@@ -4,10 +4,11 @@ import '../stylesheets/menu-lg.css'
 const LgScreenMenu = () => {
   return ( 
     <nav>
-       <ul className="antialiased hover:cursor-pointer hidden md:flex font-semibold gap-2 lg:gap-6 text-base lg:text-lg">
+       <ul className="antialiased hover:cursor-pointer hidden md:flex font-medium gap-2 lg:gap-6 xl:gap-8 text-sm lg:text-base">
           <li className="md-link"> <Link to='/hospitals'>Hospitals</Link></li>
           <li className="md-link"> <Link to='/doctors'>Doctors</Link></li>
           <li className="md-link"> <Link to='/services'>Services</Link></li>
+          <li className="md-link"> <Link to='/sign_in'>Login</Link></li>
         </ul>
     </nav>
    );
