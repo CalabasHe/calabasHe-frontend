cf40b66e72b41ca00c9852c49278ded3e1c1e011
Nii-101 <noelamptey101@gmail.com>
2024-08-31 11:30:05 +0000

    style: reducing vh of loading animation

--- calabashe-app/src/pages/FacilityProfile.jsx
+++ calabashe-app/src/pages/FacilityProfile.jsx
@@ -56,7 +56,7 @@ const FacilityProfile = () => {
 
   if (isLoading)
     return (
-      <div className="h-[100vh] w-full flex items-center justify-center">
+      <div className="h-[90vh] w-full flex items-center justify-center">
         <h1 className="text-black text-2xl sm:text-4xl font-bold animate-bounce">
           Calabas<span className="text-[#04DA8D]">he</span>
         </h1>
