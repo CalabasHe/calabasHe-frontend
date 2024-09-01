4ef021bd268320570923ae771195c446eba5dcd2
Nii-101 <noelamptey101@gmail.com>
2024-09-01 20:54:26 +0000

    style: color code and ux changes for page tags

--- calabashe-app/src/pages/DocProfile.jsx
+++ calabashe-app/src/pages/DocProfile.jsx
@@ -64,7 +64,7 @@ const DocProfile = () => {
     <>
       <Header/>
           <FadeInOut>
-            <div className="z-0 absolute top-[60px] sm:top-[73px] w-[25%]  bg-green-400 h-16 max-w-[110px] sm:max-w-[180px] sm:h-24" ></div>
+            <div className="z-0 absolute top-[60px] sm:top-[73px] w-[25%]  bg-[#04DA8D] h-16 max-w-[110px] sm:max-w-[180px] sm:h-24" ></div>
             <div className="z-0 absolute top-[120px] sm:top-[165px] h-3  w-[27%] max-w-[116px] sm:max-w-[188px] border-4 sm:border-6 border-black border-t-0 border-l-0 bg-transparent"></div>
             <aside className=" mt-[65px] sm:mt-[76px] select-none ">
               <h1 className="z-10 relative max-[460px]:mx-[15%] tracking-wider mx-[20px] sm:mx-[100px] sm:translate-y-[10px] text-base sm:text-2xl  font-bold ">
--- calabashe-app/src/pages/Doctors.jsx
+++ calabashe-app/src/pages/Doctors.jsx
@@ -9,16 +9,19 @@ const Doctors = () => {
     <div className="">
       <Header/>
           <FadeInOut>
-            <div className="z-0 absolute top-[60px] sm:top-[73px] w-[25%]  bg-green-400 h-16 max-w-[110px] sm:max-w-[180px] sm:h-24" ></div>
-            <div className="z-0 absolute top-[120px] sm:top-[165px] h-3  w-[27%] max-w-[116px] sm:max-w-[188px] border-4 sm:border-6 border-black border-t-0 border-l-0 bg-transparent"></div>
-            <aside className=" mt-[65px] sm:mt-[76px] ">
+            <aside className="max-sm:w-full max-sm:bg-[#FCFBF2] max-sm:fixed max-sm:top-0 max-sm:z-30 pb-8 sm:pb-12">
+
+            <div className="z-0 absolute top-[60px] sm:top-[73px] w-[25%] bg-[#04DA8D] h-16 max-w-[110px] sm:max-w-[180px] sm:h-24" ></div>
+            <div className=" z-0 absolute top-[120px] sm:top-[165px] h-3  w-[27%] max-w-[116px] sm:max-w-[188px] border-4 sm:border-6 border-black border-t-0 border-l-0 bg-transparent"></div>
+            <div className=" mt-[65px] sm:mt-[76px] ">
               <h1 className="z-10 relative max-[460px]:mx-[15%] tracking-wider mx-[20px] sm:mx-[100px] sm:translate-y-[10px] text-base sm:text-2xl  font-bold ">
                 Medical Doctors <br/> in  Ghana
               </h1>
-            </aside>
+            </div>
+           </aside> 
           </FadeInOut>
       <AnimatePage>
-        <main className="w-full mt-8 sm:mt-16 p-2 flex flex-col items-center ">
+        <main className="w-full mt-32 sm:mt-16 p-2 flex flex-col items-center ">
           <DoctorCard/>
         </main>
       </AnimatePage>
