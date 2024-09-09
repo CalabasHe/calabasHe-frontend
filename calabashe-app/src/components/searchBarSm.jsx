2abdf5ff0d4baa6d5033eb323ab8d937defde66d
Nii-101 <noelamptey101@gmail.com>
2024-09-09 13:43:01 +0000

    fix: search bar close button

--- calabashe-app/src/components/searchBarSm.jsx
+++ calabashe-app/src/components/searchBarSm.jsx
@@ -129,11 +129,10 @@ const SearchBarSm = ({ display, setDisplay }) => {
             placeholder="Search for Doctors, Hospitals or Services"
             spellCheck="false"
           />
-          <button className="max-md-flex w-[fit-content]" onClick={handleClose}>
-            <FaTimes
-              size={20}
-              className="fill-current text-[#205CD4]"
-            />
+          <button>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2 20L0 18L8 10L0 2L2 0L10 8L18 0L20 2L12 10L20 18L18 20L10 12L2 20Z" fill="#205CD4"/>
+            </svg>
           </button>
         </div>
         <div
