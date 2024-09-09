ba6fa545f10ae3428d37ac12d777708ec7286b05
Nii-101 <noelamptey101@gmail.com>
2024-09-09 13:25:21 +0000

    still debugging

--- calabashe-app/src/components/searchBarSm.jsx
+++ calabashe-app/src/components/searchBarSm.jsx
@@ -128,7 +128,7 @@ const SearchBarSm = ({ display, setDisplay }) => {
             placeholder="Search for Doctors, Hospitals or Services"
             spellCheck="false"
           />
-          <button className="w-[fit-content]" onClick={handleClose}>
+          <button className="bg-red-500 w-[fit-content]" onClick={handleClose}>
             <FaTimes
               size={20}
               className="text-[#205CD4]"
