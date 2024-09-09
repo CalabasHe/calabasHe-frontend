95c533bd9c10b11ce7dcca6cc6168edec0911779
Nii-101 <noelamptey101@gmail.com>
2024-09-09 13:19:02 +0000

    fix: svg bugs

--- calabashe-app/src/components/searchBarSm.jsx
+++ calabashe-app/src/components/searchBarSm.jsx
@@ -96,24 +96,27 @@ const SearchBarSm = ({ display, setDisplay }) => {
             display === "block" ? "translate-y-0" : "-translate-y-full"
           }`}
         >
-          <svg
-            tabIndex="0"
-            aria-label="Search Button"
-            className="w-[18px] h-[18px]"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M19 19L14.65 14.65M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z"
-              stroke="#205CD4"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <button>
+            <svg
+              tabIndex="0"
+              aria-label="Search Button"
+              className="w-[18px] h-[18px]"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 19L14.65 14.65M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z"
+                stroke="#205CD4"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+
+          </button>
           <input
             value={searchParam}
             onChange={handleInputChange}
@@ -125,12 +128,12 @@ const SearchBarSm = ({ display, setDisplay }) => {
             placeholder="Search for Doctors, Hospitals or Services"
             spellCheck="false"
           />
-          <FaTimes
-            size={20}
-            role="button"
-            onClick={handleClose}
-            className="text-[#205CD4]"
-          />
+          <button className="w-[fit-content]" onClick={handleClose}>
+            <FaTimes
+              size={20}
+              className="text-[#205CD4]"
+            />
+          </button>
         </div>
         <div
           id="resultsCard"
