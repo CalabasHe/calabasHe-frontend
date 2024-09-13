a6cb2c8f1a9b2e3a28a1ee3b24797876b607e559
Nii-101 <noelamptey101@gmail.com>
2024-09-13 17:52:56 +0000

    feat: working on web-view search functionality

--- calabashe-app/src/pages/Homepage.jsx
+++ calabashe-app/src/pages/Homepage.jsx
@@ -8,18 +8,18 @@ import Header from "../components/Header";
 import RecentReviews from "../components/recentReviews";
 
 const Home = () => {
+
   return (
         <div className="overflow-hidden">
           <Header/>
           <AnimatePage>
             <main className='bg-[#FCFBF2] w-full mt-8 flex flex-col gap-3 items-center'>
-              <Banner/>
+              <Banner />
 
               {/* <Review/>        */}
               {/* <p className="font-semibold text-2xl sm:text-4xl ">Heyy there!!👋😁</p>
               <p className="font-semibold text-2xl sm:text-4xl ">Welcome to:</p>
               <h1 className="z-1 mt-4 text-4xl sm:text-6xl font-black animate-bounce ">Calabas<span className="text-[#04DA8D]">He</span></h1>  */}
-              
               <ExploreCategories/>
               <RecentReviews/>
               <DoctorQuote/>
