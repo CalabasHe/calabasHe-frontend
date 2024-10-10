31f7566a3a6b0ebfd3c4dfdd861e7416b81f8bca
Nii-101 <noelamptey101@gmail.com>
2024-10-10 10:46:46 +0000

    feat: working on loading animation for search

---
+++ calabashe-app/src/components/loadingAnimation.jsx
@@ -0,0 +1,13 @@
+const LoadingAnimation = ( ) => {
+  return ( 
+    <>
+      <div className="absolute bg-white h-[100vh] w-[100vw] flex items-center justify-center">
+      <h1 className="text-black text-2xl sm:text-4xl font-bold animate-bounce">
+          Calabas<span className="text-[#04DA8D]">he</span>
+        </h1>
+      </div>
+    </>
+   );
+}
+ 
+export default LoadingAnimation;
