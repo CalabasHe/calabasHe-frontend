5111d010800afa14865e49bd7cfdd9f1703b30af
Nii-101 <noelamptey101@gmail.com>
2024-10-16 15:44:32 +0000

    created a utility for scrollbars

--- calabashe-app/tailwind.config.js
+++ calabashe-app/tailwind.config.js
@@ -11,5 +11,29 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.scrollbar-thin': {
+          '::-webkit-scrollbar': {
+            width: '12px',
+          },
+          '::-webkit-scrollbar-track': {
+            borderRadius: '12px',
+            backgroundColor: '#04DA8D',
+          },
+          '::-webkit-scrollbar-thumb': {
+            backgroundColor: '#888',
+            borderRadius: '12px',
+          },
+          '::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#555',
+          },
+          'scrollbar-width': 'thin',
+          'scrollbar-color': '#04DA8D transparent',
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover'])
+    }
+  ],
 }
