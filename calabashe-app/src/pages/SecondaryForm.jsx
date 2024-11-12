f4f5bb288655d0d3129c25ee3017819f0ca9f2a5
Nii-101 <noelamptey101@gmail.com>
2024-11-12 09:13:41 +0000

    fix: margin y for md screens

--- calabashe-app/src/pages/SecondaryForm.jsx
+++ calabashe-app/src/pages/SecondaryForm.jsx
@@ -6,7 +6,7 @@ const SecondaryForms = () => {
   return (
     <div className="2xl:container mx-auto">
       <Header />
-      <main className="my-[10vh] lg:my-[15vh] w-full  px-2 md:px-12 lg:px-16 space-y-8">
+      <main className="my-[10vh] md:my-[7vh] lg:my-[15vh] w-full  px-2 md:px-12 lg:px-16 space-y-8">
         <SecondaryFormCard/>
       </main>
     </div>
