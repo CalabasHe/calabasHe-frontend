bce303e9cb964ab306ac51904053730c40646a16
desmond123-arch <desmondopokumends2004@gmail.com>
2024-11-28 14:22:58 +0000

    modified offering images

--- calabashe-app/src/components/Offering.jsx
+++ calabashe-app/src/components/Offering.jsx
@@ -10,25 +10,25 @@ const Offering = () => {
     return (
         <div className="mb-5 w-full">
             <h1 className="text-3xl font-semibold mb-10 text-center">Our Offering</h1>
-            <div className="flex flex-col md:flex-row mx-auto w-full justify-center gap-4 md:gap-10">
+            <div className="flex flex-row md:flex-row mx-auto w-full justify-center gap-6 md:gap-10">
                 <div
-                     className='shadow-md rounded-2xl flex-col  md:mx-0 mx-auto w-[65%] md:w-[28%] min-h-[23rem] lg:w-[20%] bg-contain bg-no-repeat relative bg-[#98CBD6] bg-center'
+                     className='shadow-md rounded-2xl flex-col  md:mx-0  w-[40%]  md:w-[28%] min-h-[300px] lg:w-[20%] bg-contain bg-no-repeat relative bg-[#98CBD6] bg-center'
                     style={{ backgroundImage: `url(${consultation})` }}
                 >
-                    <div className="p-4 w-full absolute bg-white bottom-0 h-[32%] md:h-[30%] rounded-b-2xl">
-                        <h3 className="font-semibold text-xl">Instant video consultation</h3>
-                        <p>Connect within 60s</p>
+                    <div className="px-5 p-0.5 md:p-4 w-full absolute bg-white bottom-0 h-[32%] md:h-[30%] rounded-b-2xl">
+                        <h3 className="font-semibold md:text-xl">Instant video consultation</h3>
+                        <p className='text-sm mt-1 md:mt-0 md:text-md'>Connect within 60s</p>
                     </div>
                 </div>
 
                 <div
-                    className='shadow-md rounded-2xl flex-col  md:mx-0 mx-auto  w-[65%] md:w-[28%] min-h-[23rem] lg:w-[20%] bg-contain bg-no-repeat relative bg-[#98CBD6] bg-center'
+                    className='shadow-md rounded-2xl flex-col  md:mx-0 w-[40%] md:w-[28%] min-h-[300px] lg:w-[20%] bg-contain bg-no-repeat relative bg-[#98CBD6] bg-center'
                     style={{ backgroundImage: `url(${near_you})` }}
                 >
-                    <div className="absolute inset-x-0 bottom-0 p-4 bg-white rounded-b-2xl">
+                    <div className="h-[32%] md:h-[30%] absolute md:inset-x-0 bottom-0 p-0.5 md:p-4 bg-white rounded-b-2xl w-full">
                         <div className="max-w-[90%] mx-auto">
-                            <h3 className="font-semibold text-xl">Find doctors near you</h3>
-                            <p>Confirmed appointment</p>
+                            <h3 className="font-semibold  md:text-xl">Find doctors near you</h3>
+                            <p className='text-sm mt-1 md:mt-1 md:text-md'>Confirmed appointment</p>
                         </div>
                     </div>
 
