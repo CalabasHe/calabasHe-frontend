04444c9eb731b63ca56c14d7863f23a20d90e032
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-03 13:57:01 +0000

    temp

--- calabashe-app/src/pages/ProvidersLogin.jsx
+++ calabashe-app/src/pages/ProvidersLogin.jsx
@@ -4,7 +4,7 @@ import ProviderLoginForm from "../components/ProvidersLoginForm";
 
 const ProvidersLogin = () => {
   return (
-    <div className="z-50 bg-red-50 overflow-hidden relative w-full min-h-screen flex flex-col items-center justify-center pt-32">
+    <div className="z-50 bg-red-50  overflow-hidden relative w-full min-h-screen h-max flex flex-col items-center justify-center pt-32">
     <Header />
     <main>
     <ProviderLoginForm/>
