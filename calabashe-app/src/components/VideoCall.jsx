22d040c06c80a417403ae705282268410ad8f544
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-19 10:39:41 +0000

    small screens

--- calabashe-app/src/components/VideoCall.jsx
+++ calabashe-app/src/components/VideoCall.jsx
@@ -1,33 +1,34 @@
-import { CallControls, CallingState, StreamTheme, useCallStateHooks, PaginatedGridLayout } from "@stream-io/video-react-sdk";
+import { CallControls, CallingState, StreamTheme, useCallStateHooks, SpeakerLayout } from "@stream-io/video-react-sdk";
 import { toast } from "sonner";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "../stylesheets/video.css";
-
 
 const VideoCall = () => {
   const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
-  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     if (callingState === CallingState.JOINED) {
       toast.dismiss();
     }
   }, [callingState]);
+
   if (callingState === CallingState.LEFT) {
     return (
-      <div className="text-center text-2xl">
+      <div className="text-center text-2xl my-10">
         Call has ended
       </div>
-    )
+    );
   }
 
   return (
-    <StreamTheme className="str-video">
-      <PaginatedGridLayout participantsPerPage={6} />
-      <CallControls />
-    </StreamTheme>
+    <div className="video-call-container">
+      <StreamTheme className="str-video">
+        <SpeakerLayout participantsPerPage={6} />
+        <CallControls className="call-controls" />
+      </StreamTheme>
+    </div>
+  );
+};
 
-  )
-}
 export default VideoCall;
