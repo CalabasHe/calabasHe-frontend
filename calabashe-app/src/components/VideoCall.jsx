bc1b23488f7d5d869b029ad3a5807c111a112d7e
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-19 11:50:44 +0000

    test call

--- calabashe-app/src/components/VideoCall.jsx
+++ calabashe-app/src/components/VideoCall.jsx
@@ -1,34 +1,56 @@
-import { CallControls, CallingState, StreamTheme, useCallStateHooks, SpeakerLayout } from "@stream-io/video-react-sdk";
-import { toast } from "sonner";
-import { useEffect } from "react";
-import "../stylesheets/video.css";
+import "../stylesheets/video.css"
+
+import { 
+  CallControls, 
+  CallingState, 
+  StreamTheme, 
+  useCallStateHooks, 
+  SpeakerLayout,
+  PaginatedGridLayout
+} from "@stream-io/video-react-sdk"
+import { toast } from "sonner"
+import { useEffect, useState } from "react"
 
 const VideoCall = () => {
-  const { useCallCallingState } = useCallStateHooks();
-  const callingState = useCallCallingState();
+  const { useCallCallingState } = useCallStateHooks()
+  const callingState = useCallCallingState()
+  const [theme] = useState("dark")
 
   useEffect(() => {
     if (callingState === CallingState.JOINED) {
-      toast.dismiss();
+      toast.dismiss()
     }
-  }, [callingState]);
+  }, [callingState])
 
   if (callingState === CallingState.LEFT) {
     return (
-      <div className="text-center text-2xl my-10">
-        Call has ended
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="text-center">
+          <h2 className="text-2xl font-medium text-gray-800 dark:text-gray-200">Call has ended</h2>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">You can close this window</p>
+        </div>
       </div>
-    );
+    )
   }
 
   return (
-    <div className="video-call-container">
-      <StreamTheme className="str-video">
-        <SpeakerLayout participantsPerPage={6} />
-        <CallControls className="call-controls" />
+    <div className="relative min-h-screen w-full mt-14 md:mt-20 lg:mt-24">
+      <StreamTheme theme={theme}>
+        <div className="str-video__container">
+          <div className="relative h-max md:mt-7 lg:mt-2 lg:h-[calc(100vh-180px)]">
+              <SpeakerLayout participantsPerPage={2} />
+            
+          </div>
+          <div className="fixed bottom-0 md:-bottom-8 lg:bottom-4 left-0 right-0 flex justify-center pb-4 lg:pb-0">
+            <div className="rounded-full bg-gray-800/80 p-2 backdrop-blur">
+              <CallControls />
+            </div>
+          </div>
+        </div>
       </StreamTheme>
     </div>
-  );
-};
+  )
+}
 
-export default VideoCall;
+export default VideoCall
+
--- calabashe-app/src/pages/VideoCallPage.jsx
+++ calabashe-app/src/pages/VideoCallPage.jsx
@@ -75,63 +75,76 @@ const VideoCallPage = () => {
 
   return (
     <div className="relative overflow-x-hidden md:overflow-hidden bg-red-50 h-screen flex flex-col min-h-screen">
-      <Header />
-      <div className="pt-20 md:pt-32 lg:pt-16 flex items-center justify-center md:h-[90vh] w-full">
-        {call ?
-          (
+      {<Header />}
+      <div className={`${call ? 'h-[90vh]' : 'pt-20 md:pt-32 lg:pt-16'} flex items-center justify-center ${!call && 'md:h-[90vh]'} w-full`}>
+        {call ? (
+          <div className="h-screen w-full mt-12">
             <StreamVideo client={clientRef.current}>
               <StreamCall call={call}>
                 <VideoCall />
               </StreamCall>
             </StreamVideo>
-          ) :
-          (
-            <div className="flex flex-col md:flex-row w-[85%]  items-center mx-auto h-max">
-              <div className="z-30 absolute top-[5%] md:top-[30%] -right-0">
-                <svg width="87" height="143" viewBox="0 0 87 143" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M165.836 69.4802C175.608 87.3906 168.092 113.357 151.88 127.34C135.669 141.323 110.762 143.322 88.9353 142.578C67.1083 141.835 48.361 138.348 34.1125 128.387C19.864 118.425 10.1143 101.989 4.61238 83.5414C-0.857316 65.1167 -2.04702 44.7037 6.40774 28.1465C14.8625 11.5892 32.9617 -1.11216 50.4864 0.145775C68.0432 1.42632 84.9933 16.6436 106.999 29.1794C129.004 41.7153 156.064 51.5697 165.836 69.4802Z"
-                    fill="#00C67F" />
-                </svg>
-              </div>
-              <div className="z-20 absolute -top-32 md:top-0 -right-20 md:-right-8">
-                <svg width="219" height="446" viewBox="0 0 219 446" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M350.506 81.8066C376.906 116.507 394.206 151.807 407.006 200.007C419.906 248.207 428.306 309.307 401.906 359.307C375.606 409.307 314.506 448.207 256.906 444.807C199.306 441.407 145.106 395.607 95.4061 345.607C45.7061 295.607 0.606066 241.407 0.0060657 186.607C-0.593934 131.807 43.4061 76.4066 93.1061 41.8066C142.706 7.10659 198.106 -6.89342 243.506 3.20658C288.906 13.2066 324.206 47.2066 350.506 81.8066Z"
-                    fill="#FEE330" />
-                </svg>
-
-              </div>
-              <div className="w-full md:w-[45%] order-2 z-30">
-                <img src={callImage} alt="Video call" aria-hidden className="w-full " />
-              </div>
-              <div className="z-30 w-full md:w-[60%] flex gap-4 flex-col order-1 items-center md:items-start text-center md:text-start">
-                <h1 className="text-4xl md:text-5xl font-extrabold">Join instant video consultation</h1>
-                <p className="w-full md:w-[80%] leading-tight">Connect Instantly with Doctors for Personalized Video Consultations Anytime, Anywhere</p>
-
-                <div className="w-[55%] flex flex-col">
-                  <label htmlFor="Meeting" className="text-gray-500 ml-2">Meeting ID</label>
-                  <input type='text' value={callId}
-                    onChange={(e) => setCallId(e.target.value)} className="p-2 rounded-xl border-gray-400" placeholder="Eg. Booking-bffc319b-1aec-4d16-b.." />
-                </div>
-
-                <button onClick={handleJoinCall} className="w-[45%] bg-custom-yellow font-bold p-2 rounded-lg hover:bg-yellow-400">Join Call</button>
-              </div>
+          </div>
+        ) : (
+          <div className="flex flex-col md:flex-row w-[85%] items-center mx-auto h-max">
+            <div className="z-30 absolute top-[5%] md:top-[30%] -right-0">
+              <svg width="87" height="143" viewBox="0 0 87 143" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M165.836 69.4802C175.608 87.3906 168.092 113.357 151.88 127.34C135.669 141.323 110.762 143.322 88.9353 142.578C67.1083 141.835 48.361 138.348 34.1125 128.387C19.864 118.425 10.1143 101.989 4.61238 83.5414C-0.857316 65.1167 -2.04702 44.7037 6.40774 28.1465C14.8625 11.5892 32.9617 -1.11216 50.4864 0.145775C68.0432 1.42632 84.9933 16.6436 106.999 29.1794C129.004 41.7153 156.064 51.5697 165.836 69.4802Z"
+                  fill="#00C67F"
+                />
+              </svg>
             </div>
-          )}
+            <div className="z-20 absolute -top-32 md:top-0 -right-20 md:-right-8">
+              <svg width="219" height="446" viewBox="0 0 219 446" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M350.506 81.8066C376.906 116.507 394.206 151.807 407.006 200.007C419.906 248.207 428.306 309.307 401.906 359.307C375.606 409.307 314.506 448.207 256.906 444.807C199.306 441.407 145.106 395.607 95.4061 345.607C45.7061 295.607 0.606066 241.407 0.0060657 186.607C-0.593934 131.807 43.4061 76.4066 93.1061 41.8066C142.706 7.10659 198.106 -6.89342 243.506 3.20658C288.906 13.2066 324.206 47.2066 350.506 81.8066Z"
+                  fill="#FEE330"
+                />
+              </svg>
+            </div>
+            <div className="w-full md:w-[45%] order-2 z-30">
+              <img src={callImage} alt="Video call" aria-hidden className="w-full" />
+            </div>
+            <div className="z-30 w-full md:w-[60%] flex gap-4 flex-col order-1 items-center md:items-start text-center md:text-start">
+              <h1 className="text-4xl md:text-5xl font-extrabold">Join instant video consultation</h1>
+              <p className="w-full md:w-[80%] leading-tight">
+                Connect Instantly with Doctors for Personalized Video Consultations Anytime, Anywhere
+              </p>
+              <div className="w-[55%] flex flex-col">
+                <label htmlFor="Meeting" className="text-gray-500 ml-2">Meeting ID</label>
+                <input
+                  type="text"
+                  value={callId}
+                  onChange={(e) => setCallId(e.target.value)}
+                  className="p-2 rounded-xl border-gray-400"
+                  placeholder="Eg. Booking-bffc319b-1aec-4d16-b.."
+                />
+              </div>
+              <button onClick={handleJoinCall} className="w-[45%] bg-custom-yellow font-bold p-2 rounded-lg hover:bg-yellow-400">
+                Join Call
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="z-30 absolute  md:top-[60%] -left-20 md:-left-16 hidden md:block">
-          <svg width="175" height="365" viewBox="0 0 175 365" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
+      {!call && (
+        <>
+          <div className="z-30 absolute md:top-[60%] -left-20 md:-left-16 hidden md:block">
+            <svg width="175" height="365" viewBox="0 0 175 365" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
                 d="M120.533 26.0184C163.033 56.4114 179.933 126.1 172.533 188.217C165.133 250.333 133.333 304.774 90.833 334.656C48.333 364.537 -4.86694 369.654 -49.1669 360.546C-93.567 351.336 -128.867 327.799 -161.567 298.02C-194.167 268.139 -224.167 232.015 -241.867 177.676C-259.567 123.44 -264.967 51.0901 -232.267 20.6971C-199.567 -9.6959 -128.867 1.9701 -59.4669 3.30043C9.93304 4.63077 78.0331 -4.37456 120.533 26.0184Z"
-                fill="#04DA8D"/>
-          </svg>
-        </div>
-      <div className="z-30 text-center text-lg text-gray-500 mt-5 md:mt-0  flex-grow flex max-h-[200px] mb-2 w-full">
-        <p className="mt-auto w-full"> ©2024, CalabasHe All rights reserved. Privacy & Legal Policies</p>
-      </div>
+                fill="#04DA8D"
+              />
+            </svg>
+          </div>
+          <div className="z-30 text-center text-lg text-gray-500 mt-5 md:mt-0 flex-grow flex max-h-[200px] mb-2 w-full">
+            <p className="mt-auto w-full">©2024, CalabasHe All rights reserved. Privacy & Legal Policies</p>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default VideoCallPage
